a47db2b665f47e4b700ea59109efd9213625ceff
colman423 <colman0423@gmail.com>
2018-01-09 19:41:15 +0800

    update static config

--- py-mosaic-image/settings.py
+++ py-mosaic-image/settings.py
@@ -105,7 +105,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/')
 MEDIA_URL = '/'
