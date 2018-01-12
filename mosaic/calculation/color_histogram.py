dfd36bf5d9e32a47ed1d82342e820b95b0165a70
colman423 <colman0423@gmail.com>
2018-01-12 12:56:09 +0800

    HTML可以離線預覽了 還在調整CSS BUG: 怎麼選都是903 TODO: 兩邊img field的長寬會一起動、下面的button們

--- mosaic/calculation/color_histogram.py
+++ mosaic/calculation/color_histogram.py
@@ -27,20 +27,20 @@ def createData(filename):
     f.close()
 
 def doColorHistogram(pixel, width, height, xStart=0, yStart=0):
-    hArr = [0]*360
-    sArr = [0]*101
-    vArr = [0]*101
+    hArr = [0 for i in xrange(360)]
+    sArr = [0 for i in xrange(101)]
+    vArr = [0 for i in xrange(101)]
     print "info:", width, height, xStart, yStart
     pixArr = [pixel[x+xStart, y+yStart] for x in xrange(width) for y in xrange(height)]
     hsvArr = [colorsys.rgb_to_hsv(item[0]/255., item[1]/255., item[2]/255.) for item in pixArr]
 
     for item in hsvArr:
-        hAdjust = int(item[0]*360+0.5)
+        hAdjust = int(round(item[0]*360))
         if hAdjust == 360:
             hAdjust = 0
         hArr[hAdjust] += 1
-        sArr[int(item[1]*100+0.5)] += 1
-        vArr[int(item[2]*100+0.5)] += 1
+        sArr[int(round(item[1]*100))] += 1
+        vArr[int(round(item[2]*100))] += 1
 
     return hArr+sArr+vArr
 
